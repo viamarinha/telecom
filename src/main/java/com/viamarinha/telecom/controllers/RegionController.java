df3462b310ec54c92a731bf3e21bcad9fd59caf1
viamarinha <63912351+viamarinha@users.noreply.github.com>
2021-06-16 23:00:08 -0400

    Add RegionController to gitRep

---
+++ src/main/java/com/viamarinha/telecom/controllers/RegionController.java
@@ -0,0 +1,47 @@
+package com.viamarinha.telecom.controllers;
+
+import com.viamarinha.telecom.models.Region;
+import com.viamarinha.telecom.services.RegionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("regions")
+public class RegionController {
+
+
+    private RegionService regionService;
+
+    @Autowired
+    public RegionController(RegionService regionService) {
+        this.regionService = regionService;
+    }
+
+    @GetMapping("allRegions")
+    public String getAllRegions(Model model) {
+        model.addAttribute("regions", regionService.getAllRegions());
+        return "regions/allRegions";
+    }
+
+    @GetMapping("/getRegionById/{id}")
+    public String getRegionById(@PathVariable("id") int id, Model model) {
+        model.addAttribute("region", regionService.getRegionById(id));
+        return "regions/showRegion";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") int id, Model model) {
+        model.addAttribute("region", regionService.getRegionById(id));
+        return "regions/update";
+    }
+
+    @PatchMapping("/update/{id}")
+    public String update(@PathVariable("id") int id, @ModelAttribute("region") Region region) {
+        regionService.updateRegion(id, region);
+        return "redirect:/regions/allRegions";
+    }
+
+
+}
